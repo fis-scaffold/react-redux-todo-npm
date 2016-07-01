0e4a153f693a44b47b2a5fa819facad459a7165e
2betop <2betop.cn@gmail.com>
2016-07-01 23:47:33 +0800

    改成 babel

--- fis-conf.js
+++ fis-conf.js
@@ -7,18 +7,17 @@ fis.hook('commonjs', {
 });
 
 fis.match('{/modules/**.js,*.jsx}', {
-  parser: fis.plugin('typescript'),
+  // parser: fis.plugin('typescript'),
   
   // typescript 就是编译速度会很快，但是对一些 es7 的语法不支持，可以用 babel 来解决。用以下内容换掉 typescript 的parser配置就好了。
-  // parser: fis.plugin('babel-5.x', {
-  //     sourceMaps: true,
-  //     optional: ["es7.decorators", "es7.classProperties"]
-  // }),
+  parser: fis.plugin('babel-5.x', {
+      sourceMaps: true,
+      optional: ["es7.decorators", "es7.classProperties"]
+  }),
   rExt: '.js'
 });
 
 // 改用 npm 方案，而不是用 fis-components
-fis.unhook('components');
 fis.hook('node_modules');
 
 // 设置成是模块化 js
